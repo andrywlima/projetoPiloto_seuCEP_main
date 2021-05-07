814e7a030fa9db3491cc88171b090fcc5f4278a1
andrywlima <andrywmarcelo.lima@gmail.com>
2021-05-07 06:09:12 -0300

    Alteração de class para funcition

--- seucep/App.js
+++ seucep/App.js
@@ -1,136 +1,100 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import type { Node } from 'react';
 import { Text, View, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 
-export default class App extends Component {
+export default function App () {
 
-  //recebeo que foi digitado no input fica com o estado digitado
-  state = {
-    
-     cep: '',
-     //criar objeto dados
+  const [cep, setCep ] = useState("")
+  const [dados, setdados] = useState(null) 
 
-     dados: {
-       bairro:'',
-       lagradouro: '',
-       complemento: '',
-       localidade: '',
-       uf : '',
-       ddd: '',
-
-     },
-
-  };
+  
 
   //criando a instância cep , função que só envia
-  buscarCep = () => {
+  const buscarCep = () => {
     // primeiro then converte todo o response do servidor em json
     // o segundo then é os dados
-    this.setState({  // serve para limpar os dados
-      dados: {
-        bairro:'',
-        lagradouro: '',
-        complemento: '',
-        localidade: '',
-        uf : '',
-        ddd: ''
-      }
+    // requisição da api   
+       if (cep.replace("-", "" ).length != 8) {
+         alert("Digite o cpf validor por favor!")
 
-    });
-    fetch(`https://viacep.com.br/ws/${this.state.cep}/json/`).then(res => res.json()).then(data =>{
-      this.setState({
-        dados: data
-      })
-      
-    }).catch(err =>{
-      console.log(err)
-    })
+         return
+       }
 
-  }
-
-  render() {
-    return (
-      //criando um style para configuração de tela chamado de 'container' para deixar tudo enquadrado.
+    fetch(`https://viacep.com.br/ws/${cep.replace("-", "" )}/json/`)
+    .then(res => res.json())
+    .then(objeto => setdados(objeto))
+    .catch(err => alert("Tente novamente, CEP invalido!")) 
+    } 
+  //criando um style para configuração de tela chamado de 'container' para deixar tudo enquadrado.
       // utilizando uma view para os texts, textinput e button, foi criando um style cara o titulo outra para o texto em seguida subtitulo
       //um style para o input para o button
 
       // foi utilizado o TouchableOpacity pois faz com que a view responda com o toque dado ao botão
+    return (
+    
       <View style={styles.container}>
-
         <Text style={styles.title}>Quer consultar seu CEP ou {"\n"} o CEP de algum lugar do Brasil?</Text>
         <Text style={styles.text}>Digite o CEP desejado aqui!</Text>
         <TextInput
-          value={this.state.cep}
-          onChangeText={cep => { this.setState({ cep }) }}
+          value={cep}
+          onChangeText={cep =>setCep( cep)}
           style={styles.input}
           placeholder='Testando app'
           placeholderTextColor='#c3c3c3'
          />
-
         <TouchableOpacity
           style={styles.button}
-          onPress={this.buscarCep}>
+          onPress={buscarCep}>
 
           <Text style={styles.textButton}>
             Buscar
         </Text>
         </TouchableOpacity>
-      {
-        this.state.dados.localidade ?  // caso esteja errado ou vazio, limpa o campo se não, tras os dados
+           { dados != null && (
         <View style = {styles.boxdados} >
-        <Text style= {styles.textdados}>Bairro:{this.state.dados.bairro}</Text>
-        <Text style= {styles.textdados}>lagradouro:{this.state.dados.lagraduro}</Text>
-        <Text style= {styles.textdados}>complemento:{this.state.dados.complemento}</Text>
-        <Text style= {styles.textdados}>localidade:{this.state.dados.localidade}</Text>
-        <Text style= {styles.textdados}>UF:{this.state.dados.uf}</Text>
-        <Text style= {styles.textdados}>DDD da região: {this.state.dados.ddd}</Text>
-      </View> : null
-      }
-      </View>
+        <Text style= {styles.textdados}>CEP:  {dados.cep}</Text>
+        <Text style= {styles.textdados}>Bairro: {dados.bairro}</Text>
+        <Text style= {styles.textdados}>lagradouro: {dados.logradouro}</Text>
+        <Text style= {styles.textdados}>complemento: {dados.complemento}</Text>
+        <Text style= {styles.textdados}>localidade: {dados.localidade}</Text>
+        <Text style= {styles.textdados}>UF: {dados.uf}</Text>
+        <Text style= {styles.textdados}>DDD da região: {dados.ddd}</Text>
+      </View> )}
+     </View>
     );
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingVertical: 100,
     marginHorizontal: 20,
-
-
   },
 
   title: {
-
     textAlign: 'center',
     color: '#ccc999',
     fontSize: 20,
     marginBottom: 80,
     fontWeight: 'bold',
-
-
   },
 
   text: {
     fontSize: 20,
     textAlign: 'center',
-
-
   },
 
   input: {
-
     marginTop: 20,
     paddingHorizontal: 25,
     paddingVertical: 10,
     borderWidth: 1,
     borderColor: '#c3c3c3'
-
   },
 
   button: {
-
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
@@ -138,8 +102,6 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     backgroundColor: 'blue',
     marginTop: 20,
-    
-    
   },
 
   textButton : {
@@ -156,9 +118,6 @@ const styles = StyleSheet.create({
 
   textdados: {
     fontSize: 20,
-
-
   }
-
 });
 
